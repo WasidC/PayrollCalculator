9a5c3685489ce5c2edaebda0997ffa870f529c57
wasidc <wasid.chowdhury@icloud.com>
2025-09-25 11:23:00 -0400

    commit

--- src/main/java/com/pluralsight/Payroll.java
+++ src/main/java/com/pluralsight/Payroll.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Payroll {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = myScanner.nextLine();
+
+        System.out.println("Enter hours worked: ");
+        double hoursWorked = myScanner.nextDouble();
+
+        System.out.println("Enter your pay rate: ");
+        double payRate = myScanner.nextDouble();
+
+        double grosspay = hoursWorked * payRate;
+
+        System.out.println("Employee: " + name);
+        System.out.printf("Gross Pay:  $%.2f", grosspay);
+
+    }
 }
